0df0f3e82ce0372935957451e9fed218cef74935
Javier Torres <javierandrestorresreyes@gmail.com>
2018-08-21 21:05:54 -0500

    Añadidos botones para ordenamiento (aun no funcionales)

--- U1-Lenis-Palma-Torres/src/gui/OptionsPanel.java
+++ U1-Lenis-Palma-Torres/src/gui/OptionsPanel.java
@@ -14,11 +14,17 @@ public class OptionsPanel extends JPanel implements ActionListener{
 	public static final String ENTER_VALUES = "ENTER";
 	public static final String GENERATE_VALUES = "GENERATE";
 	public static final String CLEAR_VALUES = "CLEAR";
+	public static final String COUNTING_SORT = "COUNTING";
+	public static final String MERGE_SORT = "MERGE";
+	public static final String QUICK_SORT = "QUICK";
 	
 	private Window main;
 	private JButton btnEnter;
 	private JButton btnGenerate;
 	private JButton btnClear;
+	private JButton btnCounting;
+	private JButton btnMerge;
+	private JButton btnQuick;
 
 	public OptionsPanel(Window main) {
 		this.main = main;
@@ -34,12 +40,27 @@ public class OptionsPanel extends JPanel implements ActionListener{
 		btnClear = new JButton("Clear Values");
 		btnClear.setActionCommand(CLEAR_VALUES);
 		btnClear.addActionListener(this);
+		
+		btnCounting = new JButton("Counting Sort");
+		btnCounting.setActionCommand(COUNTING_SORT);
+		btnCounting.addActionListener(this);
+		
+		btnMerge = new JButton("Merge Sort");
+		btnMerge.setActionCommand(MERGE_SORT);
+		btnMerge.addActionListener(this);
+		
+		btnQuick = new JButton("Quick Sort");
+		btnQuick.setActionCommand(QUICK_SORT);
+		btnQuick.addActionListener(this);
 
-		setLayout(new GridLayout(1, 3, 10, 5));
+		setLayout(new GridLayout(2, 3, 10, 5));
 		setBorder(new EmptyBorder(5, 5, 5, 5));
 		add(btnEnter);
 		add(btnGenerate);
 		add(btnClear);
+		add(btnCounting);
+		add(btnMerge);
+		add(btnQuick);
 
 	}
 
