3954ac61c5bbaa90d2dfffade15d5e3f6f7ecd3b
Javier Torres <javierandrestorresreyes@gmail.com>
2018-08-21 21:57:12 -0500

    Implementado metodo para generar numeros con porcentaje de desorden

--- U1-Lenis-Palma-Torres/src/model/Secuency.java
+++ U1-Lenis-Palma-Torres/src/model/Secuency.java
@@ -42,7 +42,8 @@ public class Secuency {
 		for (int i = 0; i < list.size(); i++) {
 			double current = list.get(i);
 			if (current % 1 == 0) {
-				txt += String.format("%.0f", list.get(i)) + " / ";
+				txt += String.format("%.0f", list.get(i)) + " / "; // If the numb is integer, the decimals are not
+																	// printed
 			} else {
 				txt += list.get(i) + " / ";
 			}
@@ -82,14 +83,15 @@ public class Secuency {
 		for (int i = 0; i < number; i++) {
 			double random = new Random().nextDouble();
 			double result = start + (random * (end - start));
+			result = Math.random() > 0.5 ? result : Math.round(result); // There is a 50% chance that a number is whole
 			if (repeated && list.contains(result)) {
 				i--;
 			} else {
-				list.add(Math.random() > 0.5? result : Math.round(result));
+				list.add(result);
 			}
 		}
 	}
-	
+
 	/**
 	 * This method generates a given number of random decimal numbers in a given
 	 * range. These numbers are sorted. There is a 50% chance that a number is
@@ -108,13 +110,13 @@ public class Secuency {
 	 */
 	public void generateSorted(int number, double start, double end, boolean repeated) {
 		generateRandomValues(number, start, end, repeated);
-		Collections.sort(list); //TODO: Implementar metodo propio de ordenamiento
+		Collections.sort(list); // TODO: Implementar metodo propio de ordenamiento
 	}
-	
+
 	/**
 	 * This method generates a given number of random decimal numbers in a given
-	 * range. These numbers are sorted inversely. There is a 50% chance that a number is
-	 * whole<br>
+	 * range. These numbers are sorted inversely. There is a 50% chance that a
+	 * number is whole<br>
 	 * <b>Pre:</b> The list != null.<br>
 	 * <b>Post:</b> The values are added to the list.
 	 * 
@@ -130,6 +132,36 @@ public class Secuency {
 	public void generateSortedInversely(int number, double start, double end, boolean repeated) {
 		generateSorted(number, start, end, repeated);
 		Collections.reverse(list);
+	}
+
+	/**
+	 * This method generates a given number of random decimal numbers in a given
+	 * range. These numbers are disordered according to a given percentage. There is
+	 * a 50% chance that a number is whole<br>
+	 * <b>Pre:</b> The list != null.<br>
+	 * <b>Post:</b> The values are added to the list.
+	 * 
+	 * @param number
+	 *            The number of values to be generated
+	 * @param start
+	 *            The minimum value
+	 * @param end
+	 *            The maximum value
+	 * @param repeated
+	 *            True if there cannot be repeated values in the list
+	 * @param disorder
+	 *            The percentage of disorder the numbers must have. The percentage
+	 *            must be greater than 0 and less than 100
+	 */
+	public void generateDisorderedPercentage(int number, double start, double end, boolean repeated, double disorder) {
+		generateSorted(number, start, end, repeated);
+		int k = (int) (list.size() * disorder / 100); // The number of disordered values
+		int pairs = 0;
+		for (int i = 0; i < list.size()-1 && pairs < k / 2; i += 2) {
+			double temp = list.get(i);
+			list.set(i, list.get(i + 1));
+			list.set(i+1, temp);
+		}
 	}
 
 }
